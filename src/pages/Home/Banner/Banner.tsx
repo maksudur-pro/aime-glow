a04b0494de950f9db29f62436741e381384f72c4
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-11-11 13:52:21 +0600

    fixed banner section

--- src/pages/Home/Banner/Banner.tsx
+++ src/pages/Home/Banner/Banner.tsx
@@ -2,6 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-fade";
+
 import slides from "../../../assets/slides.json";
 
 const Banner = () => {
@@ -16,15 +17,18 @@ const Banner = () => {
         autoplay={{
           delay: 3000,
         }}>
-        {slides.map((data) => (
-          <SwiperSlide>
-            <div className={`banner-section  ${data.className}`}>
-              <div className="banner-details w-[60%]">
+        {slides.map((data, i) => (
+          <SwiperSlide key={i}>
+            <div
+              className={`banner-section flex items-center ${data.className}`}>
+              <div className="banner-details px-[8%] ">
                 <h5 className="mb-2 tracking-wide">{data.title}</h5>
-                <h1 className="text-[50px] lg:text-[4rem] font-semibold leading-[50px] lg:leading-[70px]">
-                  {data.heading}
-                </h1>
-                <p className="my-4 lg:w-[70%]">{data.description}</p>
+                <h1
+                  className="text-[2.8rem] lg:text-[4rem] font-semibold leading-[50px] lg:leading-[70px]"
+                  dangerouslySetInnerHTML={{ __html: data.heading }}></h1>
+                <p className="my-4 text-[10px] lg:text-[16px] lg:w-[70%]">
+                  {data.description}
+                </p>
                 <button className="btn  banner-btn">{data.btnText}</button>
               </div>
             </div>
