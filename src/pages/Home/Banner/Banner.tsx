d1dbddd1ceac0bc10ce394b3b9f41b44d84012da
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-11-26 15:28:09 +0600

    fixed banner

--- src/pages/Home/Banner/Banner.tsx
+++ src/pages/Home/Banner/Banner.tsx
@@ -7,35 +7,36 @@ import slides from "../../../assets/slides.json";
 
 const Banner = () => {
   return (
-    <>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={0}
-        modules={[Autoplay, EffectFade]}
-        effect="fade"
-        loop={true}
-        autoplay={{
-          delay: 3000,
-        }}>
-        {slides.map((data, i) => (
-          <SwiperSlide key={i}>
-            <div
-              className={`banner-section flex items-center ${data.className}`}>
-              <div className="banner-details px-[8%] ">
-                <h5 className="mb-2 tracking-wide">{data.title}</h5>
-                <h1
-                  className="text-[2.8rem] lg:text-[4rem] font-semibold leading-[50px] lg:leading-[70px]"
-                  dangerouslySetInnerHTML={{ __html: data.heading }}></h1>
-                <p className="my-4 text-[10px] lg:text-[16px] lg:w-[70%]">
-                  {data.description}
-                </p>
-                <button className="btn  banner-btn">{data.btnText}</button>
-              </div>
+    <Swiper
+      slidesPerView={1}
+      loop
+      effect="fade"
+      fadeEffect={{ crossFade: true }}
+      speed={800}
+      modules={[Autoplay, EffectFade]}
+      autoplay={{ delay: 3000 }}>
+      {slides.map((slide) => (
+        <SwiperSlide key={slide.id}>
+          <div className={`banner-section ${slide.bgClass}`}>
+            <div className="px-[8%]">
+              <h5 className="mb-2 tracking-wide text-[#7e7e7e]">
+                {slide.title}
+              </h5>
+              <h1
+                className="text-[2.8rem] lg:text-[4rem] font-semibold leading-[50px] lg:leading-[70px]"
+                dangerouslySetInnerHTML={{ __html: slide.heading }}
+              />
+              <p className="my-4 text-[10px] lg:text-[16px] lg:w-[70%] text-[#7e7e7e]">
+                {slide.description}
+              </p>
+              <button className="btn bg-black text-white py-3 px-6 lg:px-10 text-[16px]">
+                {slide.btnText}
+              </button>
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
