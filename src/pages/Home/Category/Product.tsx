bd1cb00bafa06fe054b31a2c2aa4d0c76ad7c682
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-11-27 14:58:54 +0600

    fixed products section

--- src/pages/Home/Category/Product.tsx
+++ src/pages/Home/Category/Product.tsx
@@ -30,70 +30,68 @@ const Product = () => {
             1400: { slidesPerView: 4, spaceBetween: 30 }, // Extra large desktop
           }}
           className="mt-4 relative swiper ">
-          {products
-            .filter((product) => product.id >= 5 && product.id <= 10)
-            .map((product) => (
-              <SwiperSlide key={product.id}>
-                <div className="relative">
-                  <div className="w-full relative overflow-hidden">
-                    <img
-                      src={product.img}
-                      alt={product.name}
-                      className="w-full h-full"
-                      loading="lazy"
-                    />
-                    <button
-                      aria-label="Favorites"
-                      className="absolute top-2 right-2 cursor-pointer">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-5 h-5 text-gray-700 hover:text-black">
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M21.362 5.636a5.25 5.25 0 00-7.424 0L12 
+          {products.map((product) => (
+            <SwiperSlide key={product.id}>
+              <div className="relative">
+                <div className="w-full relative overflow-hidden">
+                  <img
+                    src={product.img}
+                    alt={product.name}
+                    className="w-full h-full"
+                    loading="lazy"
+                  />
+                  <button
+                    aria-label="Favorites"
+                    className="absolute top-2 right-2 cursor-pointer">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5 text-gray-700 hover:text-black">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21.362 5.636a5.25 5.25 0 00-7.424 0L12 
                  7.574l-1.938-1.938a5.25 5.25 0 10-7.424 
                  7.424L12 21.75l9.362-9.362a5.25 5.25 
                  0 000-7.424z"
-                        />
-                      </svg>
-                    </button>
-                    <div className="min-w-12 absolute top-2 left-1.5 bg-[#899f87] text-white py-1.5 px-2 uppercase text-center text-[10px]">
-                      {product?.tag}
-                    </div>
-                    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
-                      <button className="text-[14px] md:text-[16px] lg:text-[16px] h-10 md:h-12 lg:h-12 w-[200px] md:w-[260px] lg:w-[260px] bg-white text-[#808080] font-semibold uppercase hover:bg-[#899f87] hover:text-white cursor-pointer">
-                        Add to cart
-                      </button>
-                    </div>
+                      />
+                    </svg>
+                  </button>
+                  <div className="min-w-12 absolute top-2 left-1.5 bg-[#899f87] text-white py-1.5 px-2 uppercase text-center text-[10px]">
+                    {product?.tag}
                   </div>
-                  <div className="pt-3.5">
-                    <h1 className="uppercase">{product.name}</h1>
-                    <div className="rating rating-xs">
-                      {Array.from({ length: 5 }, (_, i) => (
-                        <input
-                          key={i}
-                          type="radio"
-                          name={`rating-${product.id}`}
-                          className={`mask mask-star-2 ${
-                            i < product.rating ? "bg-orange-400" : "bg-gray-300"
-                          }`}
-                          aria-label={`${i + 1} star`}
-                          checked={i < product.rating}
-                          readOnly
-                        />
-                      ))}
-                    </div>
-
-                    <h2 className="text-[#899f87]">${product.price}</h2>
+                  <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2">
+                    <button className="text-[14px] md:text-[16px] lg:text-[16px] h-10 md:h-12 lg:h-12 w-[200px] md:w-[260px] lg:w-[260px] bg-white text-[#808080] font-semibold uppercase hover:bg-[#899f87] hover:text-white cursor-pointer">
+                      Add to cart
+                    </button>
                   </div>
                 </div>
-              </SwiperSlide>
-            ))}
+                <div className="pt-3.5">
+                  <h1 className="uppercase">{product.name}</h1>
+                  <div className="rating rating-xs">
+                    {Array.from({ length: 5 }, (_, i) => (
+                      <input
+                        key={i}
+                        type="radio"
+                        name={`rating-${product.id}`}
+                        className={`mask mask-star-2 ${
+                          i < product.rating ? "bg-orange-400" : "bg-gray-300"
+                        }`}
+                        aria-label={`${i + 1} star`}
+                        checked={i < product.rating}
+                        readOnly
+                      />
+                    ))}
+                  </div>
+
+                  <h2 className="text-[#899f87]">${product.price}</h2>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
